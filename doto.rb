2339fd80805ee61ea4c91cce787b255d9777eb9f
thom <tom@tksfz.org>
2018-07-05 21:31:37 -0700

    sha

--- doto.rb
+++ doto.rb
@@ -2,7 +2,7 @@ class Doto < Formula
   desc "Command-line work tracker"
   homepage "https://github.com/tksfz/doto2"
   url "https://github.com/tksfz/doto2/releases/download/v0.1-SNAPSHOT/doto"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "3676057480479e397ac0a785bfff20b6413b606c8a29b2d8db9b328115aa8082"
 
   bottle :unneeded
 
